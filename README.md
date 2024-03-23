df89afeb956f7da71dc40ebda2811d0bbca8a71e
Chris Burnell <me@chrisburnell.com>
2024-03-24 02:44:11 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# `paper-stamp`
+
+A Web Component to display an img as a paper stamp
+
+**[Demo](https://chrisburnell.github.io/paper-stamp/demo.html)** | **[Further reading](https://chrisburnell.com/paper-stamp/)**
+
+## Examples
+
+General usage example:
+
+```html
+<script type="module" src="paper-stamp.js"></script>
+
+<paper-stamp>
+  <img src="example.jpg" alt="">
+</paper-stamp>
+```
+
+## Features
+
+This Web Component presents an img to make it look like a paper stamp.
+
+## Installation
+
+You have a few options (choose one of these):
+
+1. Install via [npm](https://www.npmjs.com/package/@chrisburnell/paper-stamp): `npm install @chrisburnell/paper-stamp`
+1. [Download the source manually from GitHub](https://github.com/chrisburnell/paper-stamp/releases) into your project.
+1. Skip this step and use the script directly via a 3rd party CDN (not recommended for production use)
+
+### Usage
+
+Make sure you include the `<script>` in your project (choose one of these):
+
+```html
+<!-- Host yourself -->
+<script type="module" src="paper-stamp.js"></script>
+```
+
+```html
+<!-- 3rd party CDN, not recommended for production use -->
+<script
+  type="module"
+  src="https://www.unpkg.com/@chrisburnell/paper-stamp/paper-stamp.js"
+></script>
+```
+
+```html
+<!-- 3rd party CDN, not recommended for production use -->
+<script
+  type="module"
+  src="https://esm.sh/@chrisburnell/paper-stamp"
+></script>
+```
+
+## Credit
+
+With thanks to the following people:
+
+- [David Darnes](https://darn.es) for creating this [Web Component repo template](https://github.com/daviddarnes/component-template)
---
+++ paper-stamp.js
@@ -0,0 +1,114 @@
+class PaperStamp extends HTMLElement {
+  static register(tagName) {
+    if ("customElements" in window) {
+      customElements.define(tagName || "paper-stamp", PaperStamp)
+    }
+  }
+
+  static css = `
+    :host {
+      background-image:
+        radial-gradient(
+          calc(var(--stamp-perforation, 15px) / 2),
+          transparent 95%,
+          var(--stamp-color, whitesmoke)
+        ),
+        linear-gradient(var(--stamp-color, whitesmoke) 0 0);
+      background-size:
+        calc(var(--stamp-perforation, 15px) * var(--stamp-spacing, 3.5) / 2) calc(var(--stamp-perforation, 15px) * var(--stamp-spacing, 3.5) / 2),
+        calc(100% - var(--stamp-perforation, 15px) * var(--stamp-spacing, 3.5) / 2) calc(100% - var(--stamp-perforation, 15px) * var(--stamp-spacing, 3.5) / 2);
+      background-position:
+        calc(var(--stamp-perforation, 15px) * var(--stamp-spacing, 3.5) / -4) calc(var(--stamp-perforation, 15px) * var(--stamp-spacing, 3.5) / -4),
+        50%;
+      background-repeat:
+        round,
+        no-repeat;
+      max-inline-size: 100%;
+      display: inline-block;
+      padding: var(--stamp-padding, 15px);
+    }
+    :host img {
+      display: block;
+    }
+  `
+
+  static observedAttributes = ["color", "padding", "perforation", "spacing"]
+
+  connectedCallback() {
+    if (!this.querySelector("img").getAttribute("src")) {
+      console.error(`Missing \`src\` attribute!`, this)
+      return
+    }
+
+    this.init()
+  }
+
+  getBaseCSS() {
+    let sheet = new CSSStyleSheet()
+    sheet.replaceSync(PaperStamp.css)
+    return sheet
+  }
+
+  setCSS() {
+    let stylesheets = [this.getBaseCSS()]
+    if (this.hasAttribute("color")) {
+      let sheet = new CSSStyleSheet()
+      sheet.replaceSync(`
+        :host {
+          --stamp-color: ${this.getAttribute("color")};
+        }
+      `)
+      stylesheets.push(sheet)
+    }
+    if (this.hasAttribute("padding")) {
+      let sheet = new CSSStyleSheet()
+      sheet.replaceSync(`
+        :host {
+          --stamp-padding: ${this.getAttribute("padding")};
+        }
+      `)
+      stylesheets.push(sheet)
+    }
+    if (this.hasAttribute("perforation")) {
+      let sheet = new CSSStyleSheet()
+      sheet.replaceSync(`
+        :host {
+          --stamp-perforation: ${this.getAttribute("perforation")};
+        }
+      `)
+      stylesheets.push(sheet)
+    }
+    if (this.hasAttribute("spacing")) {
+      let sheet = new CSSStyleSheet()
+      sheet.replaceSync(`
+        :host {
+          --stamp-spacing: ${this.getAttribute("spacing")};
+        }
+      `)
+      stylesheets.push(sheet)
+    }
+    this.shadowRoot.adoptedStyleSheets = stylesheets
+  }
+
+  initTemplate() {
+    if (this.shadowRoot) {
+      this.setCSS()
+      return
+    }
+
+    this.attachShadow({ mode: "open" })
+
+    this.setCSS()
+
+    let template = document.createElement("template")
+    template.innerHTML = this.innerHTML
+    this.innerHTML = ""
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+  }
+
+  async init() {
+    this.initTemplate()
+  }
+}
+
+PaperStamp.register()
